4dcb685521e1a03d5b0f44c2011238c031e7004e
xycong520 <307436925@qq.com>
2016-04-14 21:54:36 +0800

    选中更新数据

--- qizhi/app/src/main/java/com/qz/qizhi/TempDataSettingActivity.java
+++ qizhi/app/src/main/java/com/qz/qizhi/TempDataSettingActivity.java
@@ -1,28 +1,22 @@
 package com.qz.qizhi;
 
 import android.app.Activity;
-import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
 import android.widget.TextView;
 
-import com.github.mikephil.charting.charts.BarChart;
 import com.github.mikephil.charting.charts.LineChart;
-import com.github.mikephil.charting.data.BarData;
-import com.github.mikephil.charting.data.BarDataSet;
-import com.github.mikephil.charting.data.BarEntry;
 import com.github.mikephil.charting.data.Entry;
 import com.github.mikephil.charting.data.LineData;
 import com.github.mikephil.charting.data.LineDataSet;
-import com.github.mikephil.charting.formatter.FillFormatter;
-import com.github.mikephil.charting.interfaces.dataprovider.LineDataProvider;
-import com.github.mikephil.charting.interfaces.datasets.IBarDataSet;
 import com.github.mikephil.charting.interfaces.datasets.ILineDataSet;
+import com.github.mikephil.charting.utils.ColorTemplate;
 
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Calendar;
-import java.util.List;
 import java.util.Random;
 
 /**
@@ -43,83 +37,62 @@ public class TempDataSettingActivity extends Activity{
     }
 
     private void init() {
-
-        setBarChartView("温度","");
+        ((CheckBox)findViewById(R.id.cbKitchen)).setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                isEnvironmentSelect = isChecked;
+                getMultiLineChart("温度","");
+            }
+        });
+        getMultiLineChart("温度", "");
     }
 
-    private void setBarChartView(String label, String description) {
-        List<String> dateString = new ArrayList<>();
-        ArrayList<BarEntry> yVals1 = new ArrayList<BarEntry>();
-        ArrayList<Entry> lineVals = new ArrayList<>();
+    boolean isEnvironmentSelect;
+    private void getMultiLineChart(String label, String description) {
+        int[] mColors = new int[] {
+                ColorTemplate.COLORFUL_COLORS[0],
+                ColorTemplate.VORDIPLOM_COLORS[1]
+        };
+        String[] time ={
+                "箱内","室内"
+        };
+        LineChart lineChart = (LineChart) findViewById(R.id.barchat);
+        ArrayList<String> xVals = new ArrayList<String>();
         Calendar calendar = Calendar.getInstance();
         int dayOfYear = calendar.get(Calendar.DAY_OF_YEAR);
-        ArrayList<IBarDataSet> dataSets = new ArrayList<IBarDataSet>();
         for (int i = 0; i < 7; i++) {
             calendar.set(Calendar.DAY_OF_YEAR, dayOfYear - i);
             String sdf = new SimpleDateFormat("MM-dd").format(calendar.getTime());
-            dateString.add(sdf);
-            int value = 0;
-            if (label.equals("温度")) {
-                value = new Random().nextInt(10) + 28;
-            } else if (label.equals("湿度")) {
-                value = new Random().nextInt(99) + 0;
-            } else {
-                value = new Random().nextInt(200);
+            xVals.add(sdf);
+        }
+        ArrayList<ILineDataSet> dataSets = new ArrayList<ILineDataSet>();
+        for (int z = 0; z < 2; z++) {
+
+            if (z==1 && !isEnvironmentSelect){
+                break;
             }
-            lineVals.add(new Entry(value, i));
-            yVals1.add(new BarEntry(value, i));
-        }
-
-
-
-        switch (label) {
-            case "出谷":
-                break;
-            case "温度":
-            case "能耗":
-            case "湿度":
-                LineChart lineChart = (LineChart) findViewById(R.id.barchat);
-                LineDataSet set1 = new LineDataSet(lineVals, label);
-                set1.setDrawCubic(true);
-                set1.setCubicIntensity(0.2f);
-                set1.setDrawFilled(true);
-                set1.setDrawCircles(false);
-                set1.setLineWidth(1.8f);
-                set1.setCircleRadius(4f);
-                set1.setCircleColor(Color.WHITE);
-                set1.setHighLightColor(Color.rgb(244, 117, 117));
-                if ("湿度".equals(label)) {
-                    set1.setColor(Color.BLUE);
-                    set1.setFillColor(Color.BLUE);
-                } else if ("温度".equals(label)) {
-                    set1.setColor(Color.RED);
-                    set1.setFillColor(Color.RED);
+            ArrayList<Entry> values = new ArrayList<Entry>();
+            for (int i = 0; i < 7; i++) {
+                int val = new Random().nextInt(4)+30;
+                if (z==0){
+                    val = new Random().nextInt(4)+14;
                 }
-                set1.setFillAlpha(100);
-                set1.setDrawHorizontalHighlightIndicator(false);
-                set1.setFillFormatter(new FillFormatter() {
-                    @Override
-                    public float getFillLinePosition(ILineDataSet dataSet, LineDataProvider dataProvider) {
-                        return -10;
-                    }
-                });
-                LineData lineData = new LineData(dateString, set1);
-                lineData.setValueTextSize(9f);
-                lineData.setDrawValues(false);
-                // set data
-                lineChart.setData(lineData);
-//                lineChart.setBackgroundColor(Color.rgb(104, 241, 175));
-                break;
-            default:
-                BarDataSet set2 = new BarDataSet(yVals1, label);
-                set2.setBarSpacePercent(35f);
-                set2.setColor(0xff33eeaa);
-                dataSets.add(set2);
-                BarData data = new BarData(dateString, dataSets);
-                data.setValueTextSize(10f);
-                BarChart barChart = (BarChart)findViewById(R.id.barchat);
-                barChart.setData(data);
-                barChart.setDescription(description);
+                values.add(new Entry((float) val, i));
+            }
+
+            LineDataSet d = new LineDataSet(values,time[z] );
+            d.setLineWidth(2.5f);
+            d.setCircleRadius(4f);
+
+            int color = mColors[z % mColors.length];
+            d.setColor(color);
+            d.setCircleColor(color);
+            dataSets.add(d);
         }
+
+        LineData data = new LineData(xVals, dataSets);
+        lineChart.setData(data);
+        lineChart.invalidate();
+
     }
 }
