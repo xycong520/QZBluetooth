d88d52241bc85275146dd3d97f61028ca61aed9b
xycong520 <307436925@qq.com>
2016-04-22 14:11:41 +0800

    取消按钮响应

--- qizhi/app/src/main/java/com/qz/qizhi/AddDeviceActivity.java
+++ qizhi/app/src/main/java/com/qz/qizhi/AddDeviceActivity.java
@@ -8,7 +8,7 @@ import android.view.View;
 /**
  * Created by Administrator on 2016/4/21.
  */
-public class AddDeviceActivity extends Activity implements View.OnClickListener{
+public class AddDeviceActivity extends Activity implements View.OnClickListener {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,17 +19,21 @@ public class AddDeviceActivity extends Activity implements View.OnClickListener{
     private void init() {
         findViewById(R.id.ivGrainBox).setOnClickListener(this);
         findViewById(R.id.ivYinshuiji).setOnClickListener(this);
+        findViewById(R.id.btCancel).setOnClickListener(this);
     }
 
     @Override
     public void onClick(View v) {
-        switch (v.getId()){
-            case  R.id.ivGrainBox:
-                startActivity(new Intent(this,BindDeviceActivity.class));
+        switch (v.getId()) {
+            case R.id.ivGrainBox:
+                startActivity(new Intent(this, BindDeviceActivity.class));
                 finish();
                 break;
-            case  R.id.ivYinshuiji:
-                startActivity(new Intent(this,BindDeviceActivity.class));
+            case R.id.ivYinshuiji:
+                startActivity(new Intent(this, BindDeviceActivity.class));
+                finish();
+                break;
+            case R.id.btCancel:
                 finish();
                 break;
         }
